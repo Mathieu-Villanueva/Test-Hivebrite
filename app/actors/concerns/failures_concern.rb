f842a2e293ec050b2a8620525ded9707174912e8
mathieu-villanueva <villanueva.mathieu@gmail.com>
2023-04-14 10:43:35 +0200

    edit a global_attribute

--- app/actors/concerns/failures_concern.rb
+++ app/actors/concerns/failures_concern.rb
@@ -3,6 +3,10 @@ module FailuresConcern
 
   private
 
+  def raise_attribute_does_not_exist_error
+    fail!(error: 'Attribute does not exist')
+  end
+
   def raise_authorization_error
     fail!(error: 'You are not authorized to do this action')
   end
---
+++ app/actors/global_attributes/edit_name.rb
@@ -0,0 +1,39 @@
+module GlobalAttributes
+  class EditName < Actor
+    include FailuresConcern
+
+    input :attribute_name, type: String
+    input :new_name, type: String
+
+    def call
+      if not_authorized?
+        raise_authorization_error 
+      end
+      
+      raise_attribute_does_not_exist_error unless global_attribute
+
+      edit_name
+      edit_custom_attributes
+    end
+
+    private
+
+    def not_authorized?
+      !GlobalAttributePolicy.new(current_user, GlobalAttribute).edit?
+    end
+    
+    def global_attribute
+      @global_attribute ||= GlobalAttribute.find_by(name: attribute_name)
+    end
+
+    def edit_name
+      global_attribute.update(name: new_name)
+    end
+
+    def edit_custom_attributes
+      global_attribute.custom_attributes.each do |custom_attribute|
+        custom_attribute.update(name: new_name)
+      end
+    end
+  end
+end
